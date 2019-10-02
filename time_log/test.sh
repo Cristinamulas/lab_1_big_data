48ef69b4e3126fae6368d52ee1cd342f6aef052e
cristinamulas <cristinamulaslopez@gmail.com>
2019-10-02 19:25:30 -0400

    change in time test.sh

--- time_log/test.sh
+++ time_log/test.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+
 ../../start.sh\
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/input/\
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/output/\
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /time_log/input/\
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../time_log/input/\
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../lab_1_big_data/time_log/input/\
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \\\
--file ../time_log/map_f.py -mapper ../time_log/map_f.py \\\
--file ../time_log/reduce_f.py -reducer ../time_log/reduce_f.py \\\
+-file ../time_log/map_f.py -mapper ../../lab_1_big_data/time_log/map_f.py \\\
+-file ../time_log/reduce_f.py -reducer ../../lab_1_big_data/time_log/reduce_f.py \\\
 -input /time_log/input/* -output /time_log/output/\
 /usr/local/hadoop/bin/hdfs dfs -cat /time_log/output/part-00000\
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/input/\
