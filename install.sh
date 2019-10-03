f5a68828345f5fbb65d9a089a612c81b66d95d14
cristinamulas <cristinamulaslopez@gmail.com>
2019-10-03 17:15:21 -0400

    final change

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ do
     if [ "$line" = "-" ]; then
         echo "Skip $line"
     else
-        ssh root@$line -n "rm -rf /mapreduce-test/ && mkdir /mapreduce-test/"
+        ssh root@$line -n "rm -rf /lab_1_big_data/ && mkdir /lab_1_big_data/"
         echo "Copy data to $line"
-        scp  /mapreduce-test/setup.py root@$line:/mapreduce-test/ && scp /mapreduce-test/manager root@$line:/mapreduce-test/ && scp /mapreduce-test/workers root@$line:/mapreduce-test/
+        scp  /lab_1_big_data/setup.py root@$line:/lab_1_big_data/ && scp /lab_1_big_data/manager root@$line:/lab_1_big_data/ && scp /lab_1_big_data/workers root@$line:/lab_1_big_data/
         echo "Setup $line"
-        ssh root@$line -n "cd /mapreduce-test/ && python3 setup.py && ntpdate time.nist.gov"
+        ssh root@$line -n "cd /lab_1_big_data/ && python3 setup.py && ntpdate time.nist.gov"
         echo "Finished config node $line"
     fi
 done
