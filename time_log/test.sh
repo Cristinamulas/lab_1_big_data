d7d5d1b0f90ab78a25f0c9abbc6169f0586832c1
cristinamulas <cristinamulaslopez@gmail.com>
2019-10-04 11:49:33 -0400

    changes in test.sh

--- time_log/test.sh
+++ time_log/test.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
 ../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /time_log/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../log_test1.txt/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /lab_1_big_data/time_log/log_test1.txt/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--files ../../map_final.py -mapper ../../map_final.py \
--files ../../time_reduce.py -reducer ../../time_reduce.py \
+-file /lab_1_big_data/time_log/map_final.py -mapper /lab_1_big_data/time_log/map_final.py \
+-file /lab_1_big_data/time_log/time_reduce.py -reducer /lab_1_big_data/time_log/time_reduce.py \
 -input /time_log/input/* -output /time_log/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /time_log/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/input/
