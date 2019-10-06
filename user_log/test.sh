15bb66356450640ea4b864e551760a0904a21e62
cristinamulas <cristinamulaslopez@gmail.com>
2019-10-05 21:48:27 -0400

    test.sh

--- user_log/test.sh
+++ user_log/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /user_log/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /user_log/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /user_log/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /lab_1_big_data/user_log/log_test1.txt/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /lab_1_big_data/log_test1.txt/user_log/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file /lab_1_big_data/user_log/ma_f.py -mapper /lab_1_big_data/user_log/map_f.py \
 -file /lab_1_big_data/user_log/user_reduce.py -reducer /lab_1_big_data/user_log/user_reduce.py \
