d1393cb9d4a7ad9fa01cae5672688a8032c055a6
cristinamulas <cristinamulaslopez@gmail.com>
2019-10-05 21:45:12 -0400

    change text file

--- time_log/test.sh
+++ time_log/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /time_log/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /time_log/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /lab_1_big_data/time_log/log_test1.txt/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /lab_1_big_data/log_test1.txt/time_log/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file /lab_1_big_data/time_log/map_final.py -mapper /lab_1_big_data/time_log/map_final.py \
 -file /lab_1_big_data/time_log/time_reduce.py -reducer /lab_1_big_data/time_log/time_reduce.py \
